b582ea05f0d4293c1137432652a253c8f4d075ae
polinabatanova <polab98@gmail.com>
2026-02-25 16:39:56 +0000

    Added attributes and constructor in Rhombus class

--- src/griffith/Rhombus.java
+++ src/griffith/Rhombus.java
@@ -1,4 +1,13 @@
 package griffith;
 
 public class Rhombus extends Shape {
+    private double side;
+    private double height;
+
+    public Rhombus(String name, double side, double height) {
+        super(name);
+        this.side = side;
+        this.height = height;
+    }
+
 }
