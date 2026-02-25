08ec044452f9e4aee651c6b20a8af520d25b597f
polinabatanova <polab98@gmail.com>
2026-02-25 16:34:44 +0000

    Added test method for circle area

--- src/griffith/ShapeTest.java
+++ src/griffith/ShapeTest.java
@@ -1,4 +1,14 @@
 package griffith;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ShapeTest {
+
+    @Test
+    public void testCircleArea() {
+        Circle c = new Circle("Circle", 3);
+        assertEquals(Math.PI * 9, c.area(), 0);
+    }
 }
