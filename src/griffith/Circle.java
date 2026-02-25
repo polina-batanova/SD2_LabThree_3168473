afb1285d0e3f34b9b3fd61f963d39e9a6d4bcbc2
polinabatanova <polab98@gmail.com>
2026-02-25 16:30:11 +0000

    Added attribute and constructor

---
+++ src/griffith/Circle.java
@@ -0,0 +1,12 @@
+package griffith;
+
+public class Circle extends Shape {
+
+    private double radius;
+
+    public Circle(String name, double radius) {
+        super(name);
+        this.radius = radius;
+    }
+
+}
