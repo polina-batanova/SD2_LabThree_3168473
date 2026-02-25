1bd24b774af2addb50baf3c024364b0dc45994d5
polinabatanova <polab98@gmail.com>
2026-02-25 16:46:44 +0000

    Added attributes and constructor

--- src/griffith/RightAngledTriangle.java
+++ src/griffith/RightAngledTriangle.java
@@ -1,4 +1,12 @@
 package griffith;
 
 public class RightAngledTriangle extends Shape {
+    private double base;
+    private double height;
+
+    public RightAngledTriangle(String name, double base, double height) {
+        super(name);
+        this.base = base;
+        this.height = height;
+    }
 }
