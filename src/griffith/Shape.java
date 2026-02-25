b6bf89c390107fb2b51c6e5beb5dca85f71cf840
polinabatanova <polab98@gmail.com>
2026-02-25 16:26:23 +0000

    Added getter and setter

--- src/griffith/Shape.java
+++ src/griffith/Shape.java
@@ -7,4 +7,11 @@ public abstract class Shape {
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
