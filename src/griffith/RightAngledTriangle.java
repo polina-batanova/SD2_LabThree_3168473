2b50c0326f31f3d735f8b85793aeb5e4a82a6504
polinabatanova <polab98@gmail.com>
2026-02-26 07:59:57 +0000

    Added comments to RightAngledTriangle class

--- src/griffith/RightAngledTriangle.java
+++ src/griffith/RightAngledTriangle.java
@@ -1,5 +1,6 @@
 package griffith;
 
+// Polina Batanova 3168473
 public class RightAngledTriangle extends Shape {
     private double base;
     private double height;
@@ -10,17 +11,20 @@ public class RightAngledTriangle extends Shape {
         this.height = height;
     }
 
+    // method for finding area
     @Override
     public double area() {
         return (base * height) / 2;
     }
 
+    // method for finding perimeter
     @Override
     public double perimeter() {
         double hypotenuse = Math.sqrt(base * base + height * height);
         return base + height + hypotenuse;
     }
 
+    // method for string representation
     @Override
     public String toString() {
         return super.toString() + " base: " + base + " height: " + height;
