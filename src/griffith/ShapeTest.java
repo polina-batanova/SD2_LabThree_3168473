6991db51a21fd9107cc8345fe9e42e08c22c81c5
polinabatanova <polab98@gmail.com>
2026-02-26 08:00:49 +0000

    Added comments Circle test methods

--- src/griffith/ShapeTest.java
+++ src/griffith/ShapeTest.java
@@ -1,5 +1,6 @@
 package griffith;
 
+// Polina Batanova 3168473
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -8,18 +9,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ShapeTest {
 
+    // test circle area
     @Test
     public void testCircleArea() {
         Circle c = new Circle("Circle", 3);
         assertEquals(Math.PI * 9, c.area(), 0);
     }
 
+    // test circle perimeter
     @Test
     public void testCirclePerimeter() {
         Circle c = new Circle("Circle", 3);
         assertEquals(2 * Math.PI * 3, c.perimeter(), 0);
     }
 
+    // test circle string representation
     @Test
     public void testCircleToString() {
         Circle c = new Circle("Circle", 3);
